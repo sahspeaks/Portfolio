5e975e7fa56498f162faf0488ee4b18053e6a1a2
Sah Abhishek Kumar <88244359+sahspeaks@users.noreply.github.com>
2022-10-07 14:55:54 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.addEventListener("click",function(e){
     if(e.target.classList.contains("gallery-item")){
         const src=e.target.getAttribute("src");
+        const title=e.target.getAttribute("alt");
+        document.querySelector(".modal-title").innerHTML=title;
         document.querySelector(".modal-img").src=src;
+
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show();
     }
